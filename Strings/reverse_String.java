c1365d134a57b69950575c4c2f62f25796ca16c4
Soumyadeep Bera <soumyadeep0230b@gmail.com>
2023-07-21 19:49:02 +0530

    Uploading the Reverse of a String file

---
+++ Strings/reverse_String.java
@@ -0,0 +1,30 @@
+package Strings;
+
+public class reverse_String {
+    public static void main(String[] args) {
+        String s = " this   is good ";
+        String res = reverseString(s);
+        System.out.println(res);
+    }
+
+    public static String reverseString(String strs) {
+        String arr[] = strs.strip().split(" ");
+        for (int i = 0; i < arr.length / 2; i++) {
+            String temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        String joined_str = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != "") {
+                joined_str += arr[i];
+                if (i < arr.length - 1) {
+                    joined_str += " ";
+                }
+            }
+
+        }
+        System.out.println(joined_str.length());
+        return joined_str;
+    }
+}
