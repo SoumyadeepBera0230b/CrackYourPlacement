1703f38d6b24e864e0bf4a31b381a527a9e3a26d
Soumyadeep Bera <soumyadeep0230b@gmail.com>
2023-07-23 21:51:37 +0530

    Day 4

---
+++ Mathematical_Problems/missing_number.java
@@ -0,0 +1,23 @@
+package Mathematical_Problems;
+
+public class missing_number {
+    public static void main(String[] args) {
+        int nums[] = { 0, 1, 3 }; // 2 is missing
+        int res = missingNumber(nums);
+        System.out.println(res);
+    }
+
+    public static int missingNumber(int[] nums) {
+        int cnt[] = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            cnt[nums[i]] += 1;
+        }
+
+        for (int i = 0; i < cnt.length; i++) {
+            if (cnt[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
